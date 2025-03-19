daef23c3e9059bbdebc404f80c32f7b4ce5a53e9
Aayush Rathore <aayu.r.2003@gmail.com>
2025-03-20 00:15:45 +0530

    🔧 Added SEO configurations

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: "https://yellowbaba.tech",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-Us",
+          es: "es-ES",
+          fr: "fr-CA",
+        },
+      },
+      xslURL: "/sitemap.xsl",
+    }),
+  ],
 });
